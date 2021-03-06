c26fbbf128c668b71b1a6d25db4e8bef498eda03
GitHubUser97 <64754902+GitHubUser97@users.noreply.github.com>
2021-03-06 13:09:42 +0100

    Update base.py

--- yfinance/base.py
+++ yfinance/base.py
@@ -41,8 +41,9 @@ from seleniumrequests import Chrome
 chrome_options = Options()
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--window-size=1920x1080")
+chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--remote-debugging-port=9222")
-chrome_driver = '/usr/lib64/chromium-browser/chromedriver'
+chrome_driver = '/usr/bin/chromedriver'
 driver = Chrome(options=chrome_options, executable_path=chrome_driver)
 
 
